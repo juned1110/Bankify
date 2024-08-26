8005d37cb8ec0ae7b71d7be2fb74bb2a4fbdd5bb
juned1110 <junedking1110@gmail.com>
2024-08-26 13:16:35 +0530

    logged in user data in variable

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,11 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Juned",
-    lastName: "Khan",
-    email: "Junedk1110@gmail.com",
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
 
   return (
     <div>
@@ -18,7 +15,7 @@ const Home = () => {
             <HeaderBox
               type="greeting"
               title="Welcome"
-              user={loggedIn?.firstName || "Guest"}
+              user={loggedIn?.name || "Guest"}
               subtext="Access and manage your accounts and transactions efficiently."
             />
             <TotalBalanceBox
